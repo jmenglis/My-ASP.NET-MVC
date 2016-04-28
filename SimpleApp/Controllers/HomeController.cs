6a674c5dcc044ba1b8427b0996b441ef4d7e302d
jmenglis <josh@joshenglish.com>
2016-04-28 15:08:48 -0600

    Made changes to use HTTPContext

--- SimpleApp/Controllers/HomeController.cs
+++ SimpleApp/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace SimpleApp.Controllers {
     public class HomeController : Controller {
         // GET: Home
         public ActionResult Index() {
-            return View();
+            return View(GetTimeStamps());
         }
         [HttpPost]
         public ActionResult Index(Color color) {
@@ -20,7 +20,12 @@ namespace SimpleApp.Controllers {
                 Votes.RecordVote(color);
             }
             ViewBag.SelectedColor = Session["color"] = color;
-            return View();
+            return View(GetTimeStamps());
+        }
+        private List<string> GetTimeStamps() {
+            return new List<string> {
+                string.Format("App timestamp: {0}", HttpContext.Application["app_timestamp"]), string.Format("Request timestamp: {0}", Session["request_timestamp"]),
+            };
         }
     }
 }
--- SimpleApp/Global.asax.cs
+++ SimpleApp/Global.asax.cs
@@ -9,10 +9,22 @@ namespace SimpleApp
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public MvcApplication() {
+            PostAcquireRequestState += (src, args) => CreateTimeStamp();
+        }
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            CreateTimeStamp();
+        }
+        private void CreateTimeStamp() {
+            string stamp = Context.Timestamp.ToLongTimeString();
+            if (Context.Session != null) {
+                Session["request_timestamp"] = stamp;
+            } else {
+                Application["app_timestamp"] = stamp;
+            }
         }
     }
 }
