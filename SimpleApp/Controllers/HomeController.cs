46492ca4820890c662230d565b6fba205b109c98
jmenglis <josh@joshenglish.com>
2016-04-28 13:08:51 -0600

    Launching Website with updates

--- SimpleApp/Controllers/HomeController.cs
+++ SimpleApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleApp.Models;
 
 namespace SimpleApp.Controllers {
     public class HomeController : Controller {
