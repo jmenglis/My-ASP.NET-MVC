f645047ae05733ae66b29167d48accc57e4a700e
jmenglis <josh@joshenglish.com>
2016-04-29 08:35:49 -0600

    Updating the website with statusCode request.

---
+++ CommonModules/InfoModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CommonModules {
+    public class InfoModule : IHttpModule {
+        public void Init(HttpApplication app) {
+            app.EndRequest += (src, args) => {
+                HttpContext ctx = HttpContext.Current;
+                ctx.Response.Write(string.Format(
+                    "<div class='alert alert-success'>URL: {0} Status: {1}</div>", ctx.Request.RawUrl, ctx.Response.StatusCode));
+            };
+        }
+        public void Dispose() {
+
+        }
+    }
+}
